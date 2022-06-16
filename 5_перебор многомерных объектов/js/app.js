5328a7e6856b222c8a48f5275b7f60ceb65abe7c
Mershana <102474997+Mershana@users.noreply.github.com>
2022-06-16 16:59:57 +0300

    multudimensional_5

---
+++ 5_перебор многомерных объектов/js/app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+
+/* Перебор многомерных объектов в JavaScript */
+
+/* Задача 1⋕js.Pm.Md.MOI.1
+Дан следующий объект: */
+/* let obj = {
+	1: {
+		1: 11,
+		2: 12,
+		3: 13,
+	},
+	2: {
+		1: 21,
+		2: 22,
+		3: 23,
+	},
+	3: {
+		1: 24,
+		2: 25,
+		3: 26,
+	},
+} */
+/* Используя циклы, найдите сумму элементов этого объекта. */
+/* let result = 0;
+for (let key in obj){
+	let subObj = obj[key];
+
+	for (let subKey in subObj){
+		result += subObj[subKey]
+	}
+}
+console.log(result);  */ //177
